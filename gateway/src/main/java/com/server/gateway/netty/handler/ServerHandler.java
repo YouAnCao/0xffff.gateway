c93bd7b85bc32a91d42d103a3eaa87c21a59090a
Lyon <lyon.cao@yahoo.com>
2021-01-27 23:48:07 +0800

    change the server handle

--- gateway/src/main/java/com/server/gateway/netty/handler/ServerHandler.java
+++ gateway/src/main/java/com/server/gateway/netty/handler/ServerHandler.java
@@ -4,17 +4,24 @@ package com.server.gateway.netty.handler;
 import com.server.ext.protocol.WireProtocol;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * create by Lyon.Cao in 2021/01/19 1:20
  **/
 public class ServerHandler extends SimpleChannelInboundHandler<WireProtocol> {
 
+    private Logger logger = LoggerFactory.getLogger(ServerHandler.class);
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, WireProtocol wireProtocol) throws Exception {
 
     }
 
-
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        logger.info("client connect.");
+        super.channelActive(ctx);
+    }
 }
