7b540fa32a65bd5609c2ea137344a78305ad2e08
Lyon <lyon.cao@yahoo.com>
2021-01-28 22:19:21 +0800

    server handler

--- gateway/src/main/java/com/server/gateway/netty/handler/ServerHandler.java
+++ gateway/src/main/java/com/server/gateway/netty/handler/ServerHandler.java
@@ -8,7 +8,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.Attribute;
-import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
